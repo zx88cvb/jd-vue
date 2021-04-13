1173e2f83edc8448bcc1f9d93d40980c6a6b70b0
zx88cvb <295389754@qq.com>
2021-04-13 22:18:49 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,85 @@
           </svg>
         </span>
       </div>
+      <div class="search">
+        <span class="iconfont">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-search"></use>
+          </svg>
+        </span>
+        <span class="search__text">
+          搜索以下
+        </span>
+      </div>
+      <div class="banner">
+        <img
+          class="banner__img"
+          alt="banner"
+          src="http://www.dell-lee.com/imgs/vue3/banner.jpg" />
+      </div>
+      <div class="icons">
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img src="http://www.dell-lee.com/imgs/vue3/超市.png"
+            alt="img"
+            class="icons__item__img" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+      </div>
+      <div class="gap"></div>
     </div>
     <div class="docker">
       <div class="docker__item docker__item--active">
@@ -85,6 +164,56 @@
     font-size: .2rem;
   }
 }
+.search {
+  margin-bottom: .12rem;
+  line-height: .32rem;
+  background: #F5F5F5;
+  color: #B7B7B7;
+  border-radius: .16rem;
+  .iconfont {
+    position: relative;
+    top: .02rem;
+    display: inline-block;
+    padding: 0 .12rem 0 .16rem;
+    font-size: .16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: .14rem;  
+  }
+}
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: .06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  margin: 0 -.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+}
 .docker {
   display: flex;
   position: absolute;
@@ -93,7 +222,7 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: .01rem solid #f1f1f1;
+  border-top: .01rem solid $content-bgColor;
   color: $content-fontcolor;
   &__item {
     flex: 1;
